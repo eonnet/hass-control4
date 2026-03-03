83d1d8beafc95200a5bb55ec664e4afe53a59c98
eonnet <53785707+eonnet@users.noreply.github.com>
2026-03-03 10:12:58 +0100

    Enable polling for thermostat state updates
    Added polling capability for thermostat updates.

--- custom_components/control4/climate.py
+++ custom_components/control4/climate.py
@@ -204,6 +204,7 @@ class Control4Climate(Control4Entity, ClimateEntity):  # type: ignore[misc]
         else:
             self._thermostat_setup = {}
         self._aux_heat_active = False
+        self._attr_should_poll = True
 
     def create_api_object(self):
         """Create a pyControl4 device object.
@@ -496,3 +497,8 @@ class Control4Climate(Control4Entity, ClimateEntity):  # type: ignore[misc]
         self._aux_heat_active = False
         if self.hvac_mode == HVACMode.HEAT:
             await self.async_set_hvac_mode(HVACMode.HEAT)
+    async def async_update(self) -> None:
+        """Poll Control4 for updated thermostat variables."""
+        self._extra_state_attributes = await director_get_entry_variables(
+            self.hass, self.entry, self._idx
+        )
